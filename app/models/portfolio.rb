b31217567e50880e4af02327282ab080581e7941
Raine <raine.ahmed@gmail.com>
2018-03-07 21:01:16 -0600

    test

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -14,4 +14,5 @@ class Portfolio < ApplicationRecord
     self.main_image ||= Placeholder.image_generator(height: '600', width: '400') 
     self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
+  #
 end
