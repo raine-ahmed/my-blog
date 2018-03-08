e0a69ea404c64e54e4d1709d592a06108f64b35a
Raine <raine.ahmed@gmail.com>
2018-03-07 22:20:21 -0600

    Integrated technology model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  has_many :technologies
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
@@ -14,5 +15,4 @@ class Portfolio < ApplicationRecord
     self.main_image ||= Placeholder.image_generator(height: '600', width: '400') 
     self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
-  #
 end
