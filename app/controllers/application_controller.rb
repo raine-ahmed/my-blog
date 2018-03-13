d2d913d1f2ddf716b15d4b9b5ec07a6c6737bd64
Raine <raine.ahmed@gmail.com>
2018-03-12 21:01:08 -0500

    Fixed before_filer issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_filter :configure_permitted_parameter, if: :devise_controller?
+  before_action :configure_permitted_parameter, if: :devise_controller?
 
   def configure_permitted_parameter
     #rails 5
